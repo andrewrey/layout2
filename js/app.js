57c06ccabe0653a0a99ecfd3ecb6f330f2552ca2
Andrew Reynolds <andrew_reynolds@mac.com>
2021-05-24 23:25:54 -0700

    added transitions for modal

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 // SELECTORS //
 const burger = document.querySelector(".header__wrap i");
 const modalBackdrop = document.querySelector(".modal_backdrop");
+const modal = document.querySelector(".modal");
 const body = document.querySelector("body");
 
 // EVENT LISTENERS //
 burger.addEventListener("click", () => {
   modalBackdrop.classList.add("darken");
+  modal.classList.add("open");
 });
 
 modalBackdrop.addEventListener("click", () => {
   modalBackdrop.classList.remove("darken");
+  modal.classList.remove("open");
 });
